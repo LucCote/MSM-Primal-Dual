2c413e2ea6f4cef2310d716d70c3d0d54fc78d8a
LucCote <hockey.luc66@gmail.com>
2023-11-11 23:35:44 -0500

    updated visualizations

--- visuals/time_visualizer.py
+++ visuals/time_visualizer.py
@@ -23,12 +23,15 @@ for i in range(len(Trials)):
     valPD_vec = []
     dual_vec = []
     valD_vec = []
+    valGLP_vec = []
     queriesG_vec = []
     queriesPD_vec = []
     queriesD_vec = []
+    queriesGLP_vec = []
     timeG_vec = []
     timePD_vec = []
     timeD_vec = []
+    timeGLP_vec = []
     k_vec = []
     n = 0
     # dual,DUAL,greedy,pd,queriesG,queriesPD,queriesD,timeG,timePD,timeD,k,n,trial
@@ -39,48 +42,59 @@ for i in range(len(Trials)):
       valPD_vec_sub = []
       dual_vec_sub = []
       valD_vec_sub = []
+      valGLP_vec_sub = []
       queriesG_vec_sub = []
       queriesPD_vec_sub = []
       queriesD_vec_sub = []
+      queriesGLP_vec_sub = []
       timeG_vec_sub = []
       timePD_vec_sub = []
       timeD_vec_sub = []
+      timeGLP_vec_sub = []
       k_vec_sub = []
       for row in reader_greedy:
         valG_vec_sub.append(float(row[2]))
         valPD_vec_sub.append(float(row[3]))
         valD_vec_sub.append(float(row[1]))
         dual_vec_sub.append(float(row[0]))
-        queriesG_vec_sub.append(float(row[4]))
-        queriesPD_vec_sub.append(float(row[5]))
-        queriesD_vec_sub.append(float(row[6]))
-        timeG_vec_sub.append(float(row[7]))
-        timePD_vec_sub.append(float(row[8]))
-        timeD_vec_sub.append(float(row[9]))
-        k_vec_sub.append(float(row[10]))
+        valGLP_vec_sub.append(float(row[4]))
+        queriesG_vec_sub.append(float(row[5]))
+        queriesPD_vec_sub.append(float(row[6]))
+        queriesD_vec_sub.append(float(row[7]))
+        queriesGLP_vec_sub.append(float(row[8]))
+        timeG_vec_sub.append(float(row[9]))
+        timePD_vec_sub.append(float(row[10]))
+        timeD_vec_sub.append(float(row[11]))
+        timeGLP_vec_sub.append(float(row[12]))
+        k_vec_sub.append(float(row[13]))
         if len(k_vec_sub) % 5 == 0:
           valG_vec.append(np.median(valG_vec_sub))
           valPD_vec.append(np.median(valPD_vec_sub))
           valD_vec.append(np.median(valD_vec_sub))
           dual_vec.append(np.median(dual_vec_sub))
+          valGLP_vec.append(np.median(valG_vec_sub))
           queriesG_vec.append(np.median(queriesG_vec_sub))
           queriesPD_vec.append(np.median(queriesPD_vec_sub))
           queriesD_vec.append(np.median(queriesD_vec_sub))
+          queriesGLP_vec.append(np.median(queriesG_vec_sub))
           timeG_vec.append(np.median(timeG_vec_sub))
           timePD_vec.append(np.median(timePD_vec_sub))
           timeD_vec.append(np.median(timeD_vec_sub))
+          timeGLP_vec.append(np.median(timeGLP_vec_sub))
           k_vec.append(np.median(k_vec_sub))
-        n = int(row[11])
+        n = int(row[14])
     
     ### Time vs. K value
     ax1 = int(i / 4)
     ax2 = i % 4
     X = k_vec
-    ax[ax1, ax2].plot(X, timePD_vec, color = 'purple', label = 'primal-dual', linestyle='-', marker='o')
-    ax[ax1, ax2].plot(X, timeD_vec, color = 'g', label = 'BQSDUAL', linestyle='-', marker='^')
-    ax[ax1, ax2].plot(X, timeG_vec, color = 'r', label = 'greedy', linestyle='-', marker='s')
-    ax[ax1, ax2].set(xlabel='k Value', ylabel='Time (seconds)')
-    ax[ax1, ax2].set_title(Trial_Names[i] + " (n=" +str(n)+")")
+
+    ax[ax1,ax2].plot(X, timePD_vec, color = 'purple', label = 'Primal-Dual', linestyle='-', marker='o')
+    ax[ax1,ax2].plot(X, timeD_vec, color = 'g', label = 'BQS', linestyle='-', marker='^')
+    ax[ax1,ax2].plot(X, timeG_vec, color = 'r', label = 'Greedy', linestyle='-', marker='s')
+    ax[ax1,ax2].plot(X, timeGLP_vec, color = 'b', label = 'GreeDual2', linestyle='-', marker='x')
+    ax[ax1,ax2].set(xlabel='k Value', ylabel='Time (seconds)')
+    ax[ax1,ax2].set_title(Trial_Names[i] + " (n=" +str(n)+")")
 
 figure.delaxes(ax[1, 3])
 ax[1][2].legend(loc='center left', bbox_to_anchor=(1.3, 0.5))
